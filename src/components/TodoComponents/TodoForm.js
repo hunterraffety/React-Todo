4bd49b8d19623a43ffe3ff5e8377c3617dc82d2d
hsrsbd <hsraffety@gmail.com>
2019-05-29 13:04:26 -0700

    updating components.

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,19 +1,21 @@
 import React from 'react';
 
-const TodoForm = () => {
-  return (
-    <div className='formContainer'>
-      <form>
-        <input type='text' placeholder='Todo Goes Here...' />
-        <button className='add' name='add'>
-          Add Todo
-        </button>
-        <button className='clear' name='clearCompleted'>
-          Clear Completed
-        </button>
-      </form>
-    </div>
-  );
-};
+class TodoForm extends React.Component {
+  render() {
+    return (
+      <div className='formContainer'>
+        <form>
+          <input type='text' placeholder='Todo Goes Here...' />
+          <button className='add' name='add'>
+            Add Todo
+          </button>
+          <button className='clear' name='clearCompleted'>
+            Clear Completed
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default TodoForm;
